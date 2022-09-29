d2f08989f6b159e9b3fe5403f266312ff6aaaee7
JcDieguez <juancruzdieguez95@gmail.com>
2022-09-29 15:07:03 -0300

    item collection 2

--- src/containers/CartContainer/index.jsx
+++ src/containers/CartContainer/index.jsx
@@ -3,37 +3,64 @@ import { Shop } from '../../context/ShopProvedor';
 import { DataGrid } from '@mui/x-data-grid';
 import { Button } from '@mui/material';
 import ordenGenerada from "../../services/ordenGenerada";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../firebase/config";
+import { doc, updateDoc, getDoc } from "firebase/firestore";
 
 
 const Cart = () => {
 
-  const {cart, removeItem, clearCart, total} = useContext(Shop);
+  const { cart, removeItem, clearCart, total } = useContext(Shop);
 
   const renderImage = (image) => {
-    return(
-      <img src={image.value} alt="cart-product" style={{height: '120px'}}></img>
+    return (
+      <img src={image.value} alt="cart-product" style={{ height: '120px' }}></img>
     )
   }
 
   const renderRemoveButton = (item) => {
     const product = item.value
     return (
-      <Button onClick={()=> removeItem(product)} variant="contained" color="error">
+      <Button onClick={() => removeItem(product)} variant="contained" color="error">
         Eliminar
       </Button>
     )
   }
 
-  const handleBuy = () => {
+  const handleBuy = async () => {
     const totalcarrito = total();
-    const orden = ordenGenerada("Juan Cruz", "jc@dieguez.com","12313213", cart, totalcarrito);
-    console.log(orden)
+    const orden = ordenGenerada("Juan Cruz", "jc@dieguez.com", "12313213", cart, totalcarrito);
+    console.log(orden);
+
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(db, "orders"), orden);
+
+
+
+    // actualizamos el stock del producto despues de generar la compra.
+    cart.forEach(async (productoEnCarrito) => {
+      
+      //Accedemos a la referencia del produc
+      const productoRef = doc(db, "products", productoEnCarrito.id);
+
+
+      //Llamamos al snapshot llamando a firebase
+      const productoSnap = await getDoc(productoRef)
+
+      //en snapshot.data nos devuelve la info del doc a actualizar
+      await updateDoc(productoRef,
+        { stock: productoSnap.data().stock - productoEnCarrito.quantity, });
+    })
+
+    alert(
+      `Gracias por su compra! La orgen generada con ID: ${docRef.id}`
+    );
   }
 
 
 
   const columns = [
-    { field: 'image', headerName: 'Imagen', width: 250, renderCell: renderImage},
+    { field: 'image', headerName: 'Imagen', width: 250, renderCell: renderImage },
     { field: 'title', headerName: 'Producto', width: 450 },
     { field: 'quantity', headerName: 'Cantidad', width: 80 },
     { field: 'price', headerName: 'Precio', width: 80 },
@@ -60,20 +87,20 @@ const Cart = () => {
 
   return (
     <>
-    {!cart.length 
-    ?<div>Carrito vacio</div>
-      :<div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={filas}
-        columns={columns}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
-        rowHeight={150}
-      />
-      <p>Total a Pagar :  {total()} </p>
-      <Button onClick={clearCart} color="error" variant="outlined">Vaciar productos</Button>
-      <button onClick={handleBuy}>Confirmar compra</button>
-    </div>}
+      {!cart.length
+        ? <div>Carrito vacio</div>
+        : <div style={{ height: 400, width: '100%' }}>
+          <DataGrid
+            rows={filas}
+            columns={columns}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+            rowHeight={150}
+          />
+          <p>Total a Pagar :  {total()} </p>
+          <Button onClick={clearCart} color="error" variant="outlined">Vaciar productos</Button>
+          <button onClick={handleBuy}>Confirmar compra</button>
+        </div>}
     </>
   );
 }
